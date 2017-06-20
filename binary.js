899cbb59543659f64cef458de15fd441f49e386d
budhip <ryo.bunraku@yahoo.com>
2017-06-20 16:19:09 +0700

    commit tugas binnary-search

--- binary.js
+++ binary.js
@@ -4,13 +4,33 @@ var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
-  // Your sorting code
-  return arr
+  //menggunakan selection sort
+  var temp = [];
+  while (arr.length > 0) {
+    var min = Math.min(...arr);
+    var indexMin = arr.indexOf(min);
+    temp.push(min);
+    arr.splice(indexMin,1);
+  }
+  return temp;
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+function binary_search (search, array) {
+  var awal = 0;
+  var akhir = array.length-1;
+  var tengah = Math.floor((awal + akhir)/2);
+  // debugger;
+  while (awal <= akhir) {
+    tengah = Math.floor((awal + akhir)/2);
+    if (search == array[tengah]) {
+      return tengah;
+    } else if (search < array[tengah]){
+      akhir = tengah - 1;
+    } else if (search > array[tengah]){
+      awal = tengah + 1;
+    }
+  }
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
