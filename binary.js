9ffdb32adc9e056dd217639ca5989a8db19dcd7b
pdvega <pdvegaa@gmail.com>
2017-06-20 12:17:14 +0700

    sorting

--- binary.js
+++ binary.js
@@ -5,6 +5,15 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
+  for(var x=1; x<arr.length; x++){
+    var y=x;
+    while (y>0 && arr[y-1] > arr[y]) {
+      var temp = arr[y];
+      arr[y] = arr[y-1];
+      arr[y-1] = temp;
+      y=y-1;
+    }
+  }
   return arr
 }
 
@@ -17,14 +26,14 @@ var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
 // Driver code
-console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
+console.log(binarySearch(8, arrayGenapSorted))
+console.log(binarySearch(10, arrayGenapSorted))
+console.log(binarySearch(33, arrayGenapSorted))
 
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
-console.log(binary_search(2, arrayGanjilSorted))
+console.log(binarySearch(53, arrayGanjilSorted))
+console.log(binarySearch(3, arrayGanjilSorted))
+console.log(binarySearch(2, arrayGanjilSorted))
 
 module.exports = {
-  binary_search
+  binarySearch
 }
