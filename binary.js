87fb5c8d39efa86a6860d40ecbdc3ab91ca46591
Ahmad Nasikin <ahmadnasikin@Ahmads-MacBook-Pro.local>
2017-06-20 21:52:13 +0700

    Tugas Binary Search

--- binary.js
+++ binary.js
@@ -3,18 +3,60 @@
 var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
+
+/*
+for i = 1 to length(A)
+    j ← i
+    while j > 0 and A[j-1] > A[j]
+        swap A[j] and A[j-1]
+        j ← j - 1
+    end while
+end for
+*/
 function ownSort(arr) {
   // Your sorting code
+  for (var i = 1; i < arr.length; i++) {
+    for (var j = 0; j < i; j++) {
+      if (arr[j] > arr[i] ) {
+        var tam = arr[j];
+        arr[j] = arr[i];
+        arr[i] = tam;
+      }
+
+    }
+  }
   return arr
 }
 
-function binarySearch (search, array) {
+
+
+function binary_search (search, array) {
   // Your searching code
-  return 0;
+  var awal = 0;
+  var akhir = array.length-1;
+  var tengah = Math.floor((awal + akhir) / 2);
+
+  while (awal <= akhir) {
+
+    tengah = Math.floor((awal + akhir) / 2);
+
+    if (search == array[tengah]) {
+      return tengah;
+    }
+
+    else if (search > array[tengah]) {
+      awal = tengah +1;
+    }
+    else if (search < array[tengah]) {
+      akhir = tengah -1;
+    }
+  }
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
+// console.log(arrayGenapSorted);
 
 // Driver code
 console.log(binary_search(8, arrayGenapSorted))
