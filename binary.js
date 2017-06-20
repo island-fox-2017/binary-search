fb234fbc539ebf1547f6fc7006037a8108608475
anggraito laras <anggraita.laras@gmail.com>
2017-06-20 19:34:14 +0700

    added new binary

--- binary.js
+++ binary.js
@@ -33,35 +33,41 @@ function ownSort(arr) {
   }
   return arr
 }
-// console.log(ownSort(testArrayGenap));
-// console.log(ownSort(testArrayGanjil));
+console.log(ownSort(testArrayGenap));
+console.log(ownSort(testArrayGanjil));
 
 function binary_search (search, array) {
-  // Your searching code
-  for(let k=0;k<array.length;k++){
-    if(array[k] == search){
-      return k;
+  var start = 0;
+  var end = array.length - 1;
+  var middle = Math.floor((start+end)/2);
+
+  while(start <= end){
+    middle = Math.floor((start+end)/2);
+    if(search < array[middle]){
+      end = middle - 1 ;
+    }
+    if(search > array[middle]){
+      start = middle + 1;
+    }
+    if(search == array[middle]){
+       return middle ;
     }
   }
-    return -1;
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
 // Driver code
+
 console.log(binary_search(8, arrayGenapSorted))
 console.log(binary_search(10, arrayGenapSorted))
 console.log(binary_search(33, arrayGenapSorted))
-// console.log(binary_search(8, arrayGenapSorted) === 0)
-// console.log(binary_search(10, arrayGenapSorted) === 1)
-// console.log(binary_search(33, arrayGenapSorted) === -1)
+
 console.log(binary_search(53, arrayGanjilSorted))
 console.log(binary_search(3, arrayGanjilSorted))
 console.log(binary_search(2, arrayGanjilSorted))
-// console.log(binary_search(53, arrayGanjilSorted) === 3)
-// console.log(binary_search(3, arrayGanjilSorted) === 0)
-// console.log(binary_search(2, arrayGanjilSorted) === -1)
 
 module.exports = {
   binary_search
