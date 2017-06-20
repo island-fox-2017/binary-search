8b9481d266d5a2d022dd38abdae76b951470ebe7
name": null, & jumadiakhmad <pair+fajarkarim.jumadiakhmad@hacktiv8.com>
2017-06-20 12:39:49 +0700

    binary DONE

--- binary.js
+++ binary.js
@@ -1,16 +1,36 @@
 'use strict'
 
-var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
+var testArrayGenap = [40, 18, 22, 32, 90, 10, 22, 8]
 var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
-  return arr
+  let temporary;
+  for (let i = 1; i < arr.length; i++) {
+    for (let j = 0; j < i; j++) {
+      if (arr[i] < arr[j]) {
+        temporary = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temporary;
+      }
+    }
+  }
+  return arr;
 }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
+function binary_search(search, arr, start = 0, end = arr.length - 1) {
+  // Your sorting code
+  if (start > end)
+    return -1;
+
+  let mid  = Math.floor( (start + end) / 2 );
+   
+  if (search > arr[mid])
+    return binary_search(search, arr, (mid + 1), end);
+  else if (search < arr[mid])
+    return binary_search(search, arr, start, (mid - 1));
+  else
+    return mid;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
