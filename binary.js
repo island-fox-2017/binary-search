35b858f620f130a9f615df43c3002837ac2ade36
adnin31 <adnin.ris31@gmail.com>
2017-06-20 19:02:10 +0700

    binnary search telah dikerjakan telah dikerjakan

--- binary.js
+++ binary.js
@@ -5,12 +5,31 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
-  return arr
+  let sort =[];
+  var pembanding;
+  while (arr.length != 0){
+        var minimumArray = Math.min(...arr)
+        sort.push(minimumArray)
+        arr.splice(arr.indexOf(minimumArray),1)
+  }
+  return sort;
 }
 
-function binarySearch (search, array) {
+function binary_search (search, array) {
   // Your searching code
-  return 0;
+  var start = array[0];
+  var end = array[array.length-1];
+  while(start <= end){
+    var mid =Math.floor((array.indexOf(end)+ array.indexOf(start))/2)
+    if (array[mid] == search) {
+        return mid;
+    }else if (array[mid] < search){
+      start = array[mid+1]
+    }else if (array[mid] > search){
+     end = array[mid-1]
+   }
+  }
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
@@ -20,7 +39,6 @@ var arrayGanjilSorted = ownSort(testArrayGanjil)
 console.log(binary_search(8, arrayGenapSorted))
 console.log(binary_search(10, arrayGenapSorted))
 console.log(binary_search(33, arrayGenapSorted))
-
 console.log(binary_search(53, arrayGanjilSorted))
 console.log(binary_search(3, arrayGanjilSorted))
 console.log(binary_search(2, arrayGanjilSorted))
