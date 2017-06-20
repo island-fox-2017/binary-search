dbfc9faa62a9a66ab3a07d86f4a4aa3992d18fa9
rusli abdul gani <rusli.gani88@gmail.com>
2017-06-20 11:50:34 +0700

    binary search done

--- binary.js
+++ binary.js
@@ -5,26 +5,49 @@ var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
 
 function ownSort(arr) {
   // Your sorting code
-  return arr
+  for (let i = 0 ; i < arr.length ; i++){
+    let key = arr[i];
+    let j = i-1;
+    while(j>=0 && key < arr[j]){
+      arr[j+1]=arr[j];
+      j--;
+    }
+    arr[j+1] = key
+  }
+  return arr;
 }
 
-function binarySearch (search, array) {
+function binarySearch(search, array) {
   // Your searching code
-  return 0;
+  let indeksAwal = 0;
+  let indeksAkhir = array.length-1;
+
+
+  while (indeksAwal <= indeksAkhir){
+    let indeksTengah = Math.floor((indeksAwal + indeksAkhir) / 2) ;
+    if (array[indeksTengah] > search){
+        indeksAkhir = indeksTengah-1;
+    }else if (array[indeksTengah] < search) {
+        indeksAwal = indeksTengah + 1;
+    }else {
+      return indeksTengah;
+    }
+  }
+  return -1;
 }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
 
 // Driver code
-console.log(binary_search(8, arrayGenapSorted))
-console.log(binary_search(10, arrayGenapSorted))
-console.log(binary_search(33, arrayGenapSorted))
+console.log(binarySearch(8, arrayGenapSorted))
+console.log(binarySearch(10, arrayGenapSorted))
+console.log(binarySearch(33, arrayGenapSorted))
 
-console.log(binary_search(53, arrayGanjilSorted))
-console.log(binary_search(3, arrayGanjilSorted))
-console.log(binary_search(2, arrayGanjilSorted))
+console.log(binarySearch(53, arrayGanjilSorted))
+console.log(binarySearch(3, arrayGanjilSorted))
+console.log(binarySearch(2, arrayGanjilSorted))
 
 module.exports = {
-  binary_search
+  binarySearch
 }
