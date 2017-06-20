a23d22c265f886b9e5fbfb01bff3771d257d0079
msrabbani <msrabbanii@gmail.com>
2017-06-20 21:49:26 +0700

    binary lagi

--- binary.js
+++ binary.js
@@ -1,17 +1,41 @@
 'use strict'
 
-var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8]
-var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55]
+var testArrayGenap = [40, 18, 22, 32, 90, 10, 10, 22, 8] // [ 8, 10, 10, 18, 22, 22, 32, 40, 90 ]
+var testArrayGanjil = [3, 31, 89, 53, 53, 85, 77, 21, 55] // [ 3, 21, 31, 53, 53, 55, 77, 85, 89 ]
 
 function ownSort(arr) {
-  // Your sorting code
-  return arr
-}
+  let swap = 0;
+  for (var i = 1; i <=arr.length; i++) {
+    for (var j = 0; j < i; j++) {
+      if (arr[i] < arr[j]) {
+        swap = arr[i]
+        arr[i] = arr[j]
+        arr[j] = swap
+      }
+    }
+  }
+    console.log(arr);
+    return arr
+  }
 
-function binarySearch (search, array) {
-  // Your searching code
-  return 0;
-}
+function binary_search (search, array) {
+  let awal = 0;
+  let akhir = array.length-1;
+  // let tamp = 0;
+
+  while (awal<=akhir) {
+    let tengah = Math.floor(akhir-awal/2)
+     if (array[tengah]<search) {
+       awal = tengah + 1
+     }else if(array[tengah]>search){
+       akhir = tengah -1
+     }else {
+       return tengah
+     }
+   }
+
+   return -1;
+ }
 
 var arrayGenapSorted = ownSort(testArrayGenap)
 var arrayGanjilSorted = ownSort(testArrayGanjil)
@@ -20,7 +44,6 @@ var arrayGanjilSorted = ownSort(testArrayGanjil)
 console.log(binary_search(8, arrayGenapSorted))
 console.log(binary_search(10, arrayGenapSorted))
 console.log(binary_search(33, arrayGenapSorted))
-
 console.log(binary_search(53, arrayGanjilSorted))
 console.log(binary_search(3, arrayGanjilSorted))
 console.log(binary_search(2, arrayGanjilSorted))
